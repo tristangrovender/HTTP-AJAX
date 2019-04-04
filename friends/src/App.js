c384cb31c884b7496619489ffd328e9a0f7df631
Tristan Grovender <therealtristangrovender@gmail.com>
2019-04-04 16:35:58 -0500

    component partially built out

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      //something here
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => {
+        this.setState({ items: res.data });
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
-      <div>
-        Stuff goes here
+      <div className="App">
+        Friends!
       </div>
     );
   }
